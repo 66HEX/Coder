7e7f8bc13a2b5a6d8a8a5dd0de1aae965f1a43d2
66HEX <hextheprod@gmail.com>
2024-11-10 01:55:49 +0100

    font issues
    font issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack(config) {
-        // Add SVG loader with @svgr/webpack
         config.module.rules.push({
             test: /\.svg$/,
             use: ['@svgr/webpack'],
         });
+
         return config;
     },
 };
