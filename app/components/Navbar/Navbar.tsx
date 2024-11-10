640ee560bb984a2a7cce1baa09a06150d3b1548a
66HEX <hextheprod@gmail.com>
2024-11-10 12:41:39 +0100

    ...

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
             gsap.from(childSplit.current.lines, { y: "100%" });
 
             gsap.to(childSplit.current.lines, {
-                y: direction === "in" ? "-100%" : "0%",
+                y: direction === "in" ? "0%" : "100%",
                 duration: 0.8,
                 ease: "customEase",
             });
