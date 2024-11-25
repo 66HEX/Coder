a576511a11809d63d7d3ac8ba0eca9527d72603d
66HEX <hextheprod@gmail.com>
2024-11-25 14:04:06 +0100

    unused import hotfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 import SmoothScrolling from "@/app/components/SmoothScrolling/SmoothScrolling";
 import Menu from "@/app/components/Menu/Menu";
-import Loader from "@/app/components/Loader";
-
 
 export const metadata: Metadata = {
     title: "Hex - Freelance Web Developer",
     description: "Portfolio of HEX, a freelance web developer specializing in modern web technologies.",
     keywords: "freelance web developer, web development, portfolio",
 };
-
 
 export default function RootLayout({
   children,
